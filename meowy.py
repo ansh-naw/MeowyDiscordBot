c72e3c7ef78c95edd1e2300737f4d6a656cd1fc1
Ansh <ansh.a.3112@gmail.com>
2021-09-20 11:05:56 +0530

    make it better

---
+++ meowy.py
@@ -0,0 +1,22 @@
+import discord
+import requests
+from discord.ext import tasks
+client = discord.Client()
+
+
+
+@client.event
+async def on_message(message):
+     prefix="!"   
+     if message.content.startswith(prefix + 'cat'):
+            @tasks.loop(seconds = 10) # repeat after every 10 seconds
+            async def myLoop():
+             response = requests.get('https://aws.random.cat/meow')
+             data = response.json()
+             await message.channel.send(data['file'])
+            myLoop.start()
+     if message.content.startswith(prefix + "stop"):
+          
+               myloop.stop()
+     
+client.run('ODg5MDY4Mzc0NzUwMDExNDAz.YUb3cg.PeualFHCwvkUPVv_VDoIgbeSqjw')
